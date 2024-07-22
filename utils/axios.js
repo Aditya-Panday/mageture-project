0bbdfa3d3845418b14eb7a42581cdbebec7bb905
Aditya-Panday <adityapanday459@gmail.com>
2024-07-22 09:37:15 +0530

    updated code

--- utils/axios.js
+++ utils/axios.js
@@ -4,12 +4,28 @@ const axiosConfig = {
   baseURL: process.env.BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   },
 };
 const api = axios.create(axiosConfig);
 
 export const post = async (url, jsonObj, headers = {}) => {
-  return api.post(url, jsonObj, { headers }).then(
+  return api
+    .post(url, jsonObj, { headers: { ...axiosConfig.headers, ...headers } })
+    .then(
+      (response) => response,
+      (error) => {
+        throw error.response ? error.response : error;
+      }
+    );
+};
+
+export const get = async (url, headers = {}) => {
+  return api.get(url, { headers: { ...axiosConfig.headers, ...headers } }).then(
     (response) => response,
     (error) => {
       throw error.response ? error.response : error;
@@ -17,29 +33,27 @@ export const post = async (url, jsonObj, headers = {}) => {
   );
 };
 
-export const get = async (url, headers) => {
-  return api.get(url, { headers }).then(
-    (response) => response,
-    (error) => {
-      throw error.response ? error.response : error;
-    }
-  );
+export const put = async (url, jsonObj, headers = {}) => {
+  return api
+    .put(url, jsonObj, { headers: { ...axiosConfig.headers, ...headers } })
+    .then(
+      (response) => response,
+      (error) => {
+        throw error.response ? error.response : error;
+      }
+    );
 };
 
-export const put = async (url, jsonObj, headers) => {
-  return api.put(url, jsonObj, { headers }).then(
-    (response) => response,
-    (error) => {
-      throw error.response ? error.response : error;
-    }
-  );
-};
-
-export const del = async (url, jsonObj, headers) => {
-  return api.delete(url, jsonObj, { headers }).then(
-    (response) => response,
-    (error) => {
-      throw error.response ? error.response : error;
-    }
-  );
+export const del = async (url, jsonObj, headers = {}) => {
+  return api
+    .delete(url, {
+      headers: { ...axiosConfig.headers, ...headers },
+      data: jsonObj,
+    })
+    .then(
+      (response) => response,
+      (error) => {
+        throw error.response ? error.response : error;
+      }
+    );
 };
