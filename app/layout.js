6793715772a128eb1cbb1c9ca1d4a038781f56a0
Aditya-Panday <adityapanday459@gmail.com>
2024-08-12 16:42:08 +0530

    updated code

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/logo.png" />
+      <meta
+        name="description"
+        content="Discover Back to Basics Education - Your partner in transforming learning into a passion. Explore our wide range of educational resources, courses, and guides designed to empower students and educators alike. Start your journey towards a brighter future today!"
+      />
+      <meta
+        name="keywords"
+        content="Education, Learning, Online Courses, Educational Resources, Student Empowerment"
+      />
+      <meta name="author" content="Back to Basics Education" />
 
       <body className={inter.className}>{children}</body>
     </html>
